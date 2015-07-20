e4b5e6ad997834dac356730e4bac823eeb68b61c
Orion Engleton - MBAir <orion.engleton@gmail.com>
2015-07-20 16:02:34 -0700

    redirect proper tests

--- test/worker_test.exs
+++ test/worker_test.exs
@@ -5,9 +5,9 @@ defmodule WorkerTest do
 
   @message """
   Orion, 
-  This is a message that came it.. today. LOL check this out http://www.orionengleton.com Its your new website.
+  This is a message that came it.. today. LOL check this out http://github.com/rsgrafx/link_extractor Its your new website.
   """
-  @expected_link %Link{url: "http://www.orionengleton.com", title: "Learning to Dream in Color and Code. | Orion Engleton - Web Developer specializing in Ruby, AngularJS and" }
+  @expected_link %Link{url: "http://github.com/rsgrafx/link_extractor", title: "rsgrafx/link_extractor · GitHub" }
 
   test "extract_links" do 
     assert LinkExtractor.Worker.extract_links(@message) == [@expected_link]
