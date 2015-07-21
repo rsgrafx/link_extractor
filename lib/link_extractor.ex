61ba8c9d5ef9ff143e02447f488f3d6b51376746
Orion Engleton - MBAir <orion.engleton@gmail.com>
2015-07-20 21:07:43 -0700

    Added handle_link that initiates poolboy that farms out the workers - hanldler_pool options added to start function

--- lib/link_extractor.ex
+++ lib/link_extractor.ex
@@ -1,7 +1,6 @@
 defmodule LinkExtractor do
   use Application
 
-
   def inject( message ) do
     :poolboy.transaction(:link_extractor_pool, fn( worker ) ->
         LinkExtractor.Worker.handle_message( worker, message )
@@ -11,10 +10,24 @@ defmodule LinkExtractor do
   def get_links do
     Agent.get(:collector, &(&1))
   end
+
+  def handle_link(link) do
+    :poolboy.transaction(:link_extractor_link_handler_pool, fn(worker) ->
+      LinkExtractor.LinkHandler.handle_link(worker, link)
+    end)
+  end
+
   # See http://elixir-lang.org/docs/stable/elixir/Application.html
   # for more information on OTP Applications
   def start(_type, _args) do
     import Supervisor.Spec, warn: false
+
+    link_handler_pool_options = [
+      name: {:local, :link_extractor_link_handler_pool},
+      worker_module: LinkExtractor.LinkHandler,
+      size: 5,
+      max_overflow: 10
+    ]
 
     pool_options = [
       name: {:local, :link_extractor_pool}, 
@@ -25,6 +38,7 @@ defmodule LinkExtractor do
 
     children = [
       :poolboy.child_spec(:link_extractor_pool, pool_options, []),
+      :poolboy.child_spec(:link_extractor_link_handler_pool, link_handler_pool_options, []),
       worker(Agent, [fn -> [] end, [name: :collector]])
     ]
 
