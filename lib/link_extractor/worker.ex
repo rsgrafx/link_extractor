d58f66174a3b5a70784eacbbf2de4188e4c5dfd3
Orion Engleton - MBAir <orion.engleton@gmail.com>
2015-07-20 21:10:45 -0700

    Moved some logic to its own workers

--- lib/link_extractor/worker.ex
+++ lib/link_extractor/worker.ex
@@ -1,6 +1,10 @@
 defmodule LinkExtractor.Worker do
   use GenServer
 
+  alias LinkExtractor.LinkHandler, as: Handler
+  alias LinkExtractor.Link
+  @url_regex ~r(https?://[^ $\n]*)
+
   def start_link(_options) do
     GenServer.start_link(__MODULE__, :ok, [])
   end
@@ -9,52 +13,30 @@ defmodule LinkExtractor.Worker do
     {:ok, []}
   end
 
-  alias LinkExtractor.Link
-
-  @url_regex ~r(https?://[^ $\n]*)
+  def handle_message( server, message ) do 
+    GenServer.call( server, { :handle_message, message } )
+  end
 
   def extract_links(message) do 
     Regex.scan(@url_regex, message) 
     |> Enum.map( &hd/1 )
-    |> Enum.map(  &(%Link{url: &1}) )
-    |> Enum.map( &add_title/1 )
+    |> Enum.map(  &( check_valid(&1) ) )
+    |> Enum.filter( fn(x) -> x != nil end)
   end
 
-  def handle_message( server, message ) do 
-    GenServer.cast( server, { :handle_message, message } )
+  defp check_valid(url) do 
+    case HTTPoison.get(url) do 
+      {:ok, _ }   -> %Link{url: url}
+      {:error, _} -> nil
+    end
   end
 
-  def handle_cast( {:handle_message, message}, state) do 
+  def handle_call({:handle_message, message}, _from, state) do
     extract_links(message)
-    |> Enum.map( fn link -> 
-      Agent.update(:collector, &([link|&1]))
+    |> Enum.map(fn link ->
+      spawn(LinkExtractor, :handle_link, [link])
     end)
-    {:noreply, state}
+    {:reply, :ok, state}
   end
 
-  defp add_title(link = %Link{ url: url } ) do 
-    title_pattern = ~r"<title>([^<]*)</title>"
-    body = get_body( url )
-    title = Regex.run(title_pattern, body) 
-            |> Enum.at(1)
-    %Link{ link | title: title  }
-  end
-
-  defp get_body(url) do
-    response = HTTPoison.get(url)
-    {:ok, %HTTPoison.Response{body: body}} = follow_redirects(response)
-    # {:ok, %HTTPoison.Response{ body: body}} = follow_redirects(response)
-    body
-  end
-
-  defp follow_redirects(response={:ok, %HTTPoison.Response{status_code: 200}}) do 
-    response
-  end
-
-  defp follow_redirects({:ok, %HTTPoison.Response{status_code: 301, headers: header_list }}) do
-    {"Location", location}= List.keyfind(header_list, "Location", 0)
-    response = HTTPoison.get(location)
-    follow_redirects(response)
-  end
-  
-end  
+end
